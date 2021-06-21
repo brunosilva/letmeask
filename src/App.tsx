54d29087130f5ef1706b1fc7cea4db1394beeceb
Bruno Silva <brs.rsilva@gmail.com>
2021-06-21 16:58:18 -0300

    firebase connection

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
+import { Button } from "./components/Button";
+
 function App() {
   return (
-    <h1>Hello word</h1>
+    <div>
+      <Button />
+      <Button />
+      <Button />
+      <Button />
+    </div>
   );
 }
 
