1a31c346d9ee054f8a00e1e8ffb69c885fbe1229
Bruno Silva <brs.rsilva@gmail.com>
2021-06-29 20:42:04 -0300

    react route

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import { Button } from "./components/Button";
+import { BrowserRouter, Route } from 'react-router-dom';
+
 import { NewRoom } from "./pages/NewRoom";
-// import { Home } from "./pages/Home";
+import { Home } from "./pages/Home";
 
 function App() {
   return (
-      <NewRoom />
+    <BrowserRouter>
+      <Route path="/" exact component={Home} />
+      <Route path="/rooms/new" component={NewRoom} />
+    </BrowserRouter>
   );
 }
 
